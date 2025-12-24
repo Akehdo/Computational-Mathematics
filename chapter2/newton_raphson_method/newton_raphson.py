4925628f860fc2e3b6bb6865ec81bd13d507d44e
Akendo <ruslankadirov1337@gmail.com>
2025-12-24 13:34:56 +0500

    add example for newton-raphson

--- chapter2/newton_raphson_method/newton_raphson.py
+++ chapter2/newton_raphson_method/newton_raphson.py
@@ -11,10 +11,18 @@ def newton_raphson(f, df, x0, eps=1e-6, Nmax=100):
         x_next = x - fx / dfx
         print(f"n={n}, x={x}, x_next={x_next}")
 
-        if abs(x_next) < eps:
+        if abs(x_next - x) < eps:
             return x_next
 
         x = x_next
 
     raise RuntimeError("failed to converge")
 
+def f(x):
+    return x**3 - 2*x - 5
+
+def df(x):
+    return 3*x**2 - 2
+
+root = newton_raphson(f, df, 2)
+print("Approximate root:", root)
